ae512bfef97d1d1b0a4bab5f80cada25feab15b1
Matthew Faraz Tootoonchi <44936371+mtootoonchi@users.noreply.github.com>
2019-03-06 15:12:31 -0800

    Add files via upload

---
+++ AtCounter Project/AtCounter/AtCounter/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCounter
+{
+    /*
+     * AtCounter and MyProgram by Matthew Tootoonchi
+ 
+     * 
+     * AtCounter will create a 2D array filled with '@' or '-' randomly with dimensions of user input then
+     * find the number of '@' next to each other of user input, then after the user stops the program it will 
+     * finally print out the created 2D array.
+     * 
+     */
+     public class MyProgram
+     {
+         public static void Main(string[] args)
+         {
+             Console.WriteLine("AtCounter will create a 2D array filled with \'@\' or \'-\' \nrandomly with dimensions of user input then find the number \nof \'@\' next to each other of user input, then after the \nuser stops the program it will finally print out the \ncreated 2D array.\n\n");
+             Console.Write("How many rows do you want in your matrix: ");
+             int a = checkInt(Console.ReadLine());    //The rows in our 2D array.
+             Console.Write("\nHow many columns do you want in your matrix: ");
+             int b = checkInt(Console.ReadLine());    //The columns in our 2D array.
+             if (a < 0 || b < 0)
+             {
+                 Console.WriteLine("\n\nYou have stopped the program or you put an invalid input.\n");
+             }
+             else
+             {
+                 matrix num = new matrix(a, b);  //Creates a 2D array filled randomly with '@' or '-' with dimensions of user input
+                 int row = 0;
+                 int col = 0;
+                 while (true)   //Keeps asking the user where in the 2D array you want to search for '@' until the user stops it.
+                 {
+                     Console.Write("\n\nWhat is the row you want to look at\nor type \"0\" or lower to stop: ");
+                     row = checkInt(Console.ReadLine()) - 1;  //Tells the user which row you want to look at from 1 to a;
+                     if (row < 0 || row > a - 1)  //Array out of bounds case and user end.
+                     {
+                         Console.WriteLine("\n\nYou have stopped the program or you put an invalid input.\nThe matrix looks like this.\n");
+                         break;
+                     }
+                     else
+                     {
+                         Console.Write("\nWhat is the column you want to look at\nor type \"0\" or lower to stop: ");
+                         col = checkInt(Console.ReadLine()) - 1;   //Tells the user which column you want to look at from 1 to b;
+                         if (col < 0 || col > b - 1)   //Array out of bounds case and user end.
+                         {
+                             Console.WriteLine("\n\nYou have stopped the program or you put an invalid input.\nThe matrix looks like this.\n");
+                             break;
+                         }
+                         else
+                         {
+                             Console.WriteLine("\nYou have " + num.point(row, col) + "@ next to each other.");   //Prints the number of @ together.
+                         }
+                     }
+                 }
+                 num.printmatrix();   //Prints randomly generated 2D array.
+             }
+         }
+         private static int checkInt(string a)
+         {
+             if (int.TryParse(a, out int b))
+             {
+                 return Convert.ToInt32(a);
+             }
+             else
+             {
+                 return -1;
+             }
+         }
+     }
+}
---
+++ AtCounter Project/AtCounter/AtCounter/matrix.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCounter
+{
+    /*
+     * matrix by Matthew Tootoonchi
+     * 
+     * Creates a 2D array filled randomly with @ or - and has a method/function which finds how much @ are next 
+     * to each other of user input and has another method/function that prints out the created 2D array.
+     * 
+     * The dimensions of tix and checked are the same.
+     */
+    public class matrix
+    {
+        private char[,] tix;  //The randomly filled 2D array of '@' and '-'.
+        private bool[,] AtTracker;  //While counting the number of '@' in the 2D array this 2D array keeps track of each '@' so it doesn't count it again.
+        private int Mrow;   //The number of rows in the 2D array.
+        private int Mcol;   //The number of columns in the 2D array.
+
+        /*
+         * Creates a 2D array filled randomly with '@' or '-' and declares the max number of rows and columns and also declares checked.
+         */
+        public matrix(int a, int b)
+        {
+            this.Mrow = a;   //Declares the max number of rows.
+            this.Mcol = b;   //Declares the max number of columns.
+            tix = new char[a, b];
+            Random ran = new Random();
+            for (int i = 0; i < a; i++)   //Creates a 2D array named tix filled randomly with '@' or '-'.
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    if (ran.Next(0, 2) == 1)
+                    {
+                        tix[i, j] = '@';
+                    }
+                    else
+                    {
+                        tix[i, j] = '-';
+                    }
+                }
+            }
+            AtTracker = new bool[a, b];
+        }
+        /*
+         * Prints the 2D array tix.
+         */
+        public void printmatrix()
+        {
+            for (int i = 0; i < Mrow; i++)
+            {
+                for (int j = 0; j < Mcol; j++)
+                {
+                    Console.Write(tix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        /*
+         * Fills the 2D array checked with "false" and calls check to check if there is a '@'.
+         */
+        public int point(int a, int b)
+        {
+            for (int i = 0; i < Mrow; i++)   //Fills the 2D array checked with "false."
+            {
+                for (int j = 0; j < Mcol; j++)
+                {
+                    AtTracker[i, j] = false;
+                }
+            }
+            return check(a, b);   //Calls check to check if there is a '@'.
+        }
+        /*
+         * Checks if there is a '@' and uses recursion to add them all up.
+         */
+        private int check(int a, int b)
+        {
+            if (a < 0 || b < 0 || a > Mrow - 1 || b > Mcol - 1 || tix[a, b] == '-' || AtTracker[a, b])  //Checks if there is the array is out of bounds, there isn't a '@' at tix, and if the space has been checked.
+            {
+                 return 0;
+            }
+            else
+            {
+                 AtTracker[a, b]=true;   //Keeps track of the spaces checked.
+                 return check(a-1, b)+check(a+1, b)+check(a, b+1)+check(a, b-1)+1;   //Uses recursion to call the left, right, up, and down of the 2D array tix if there is a '@' and adds it all by 1.
+            }
+        }
+        /*
+         * Returns an empty toString().
+         */
+        public override string ToString()
+        {
+             return "";
+        }
+    }
+}
